29c925751440faea3b477cb5eb55d35cf5387c3d
fadulgabriel <fadulgabriel@gmail.com>
2026-08-07 14:30:02 -0300

    Forçando deploy no Vercel

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,5 +1,5 @@
 export const skills = {
-  estatistica: ['Python', 'R', 'SAS', 'Modelagem Preditiva', 'Teste de Hipóteses'],
+  estatistica: ['Python', 'R', 'SAS', 'Modelagem', 'Teste de Hipóteses'],
   data_bi: ['SQL', 'Power BI', 'Excel', 'R Shiny', 'LaTeX'],
-  ferramentas: ['Git', 'WordPress', 'Google Ads', 'Figma', 'Make', 'N8N']
+  ferramentas: ['Git', 'WordPress', 'Google Ads', 'Canva', 'Make', 'Pipefy']
 }
